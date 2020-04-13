a81452101d00892b6666fadd3b7c073d88e879cd
Pierre W <pierre.woj@hotmail.fr>
2020-04-13 11:37:09 +0200

    V 0.2 BDD Update

--- piscine.sql
+++ piscine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 13, 2020 at 08:45 AM
+-- Generation Time: Apr 13, 2020 at 09:36 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -31,11 +31,11 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `acheteur`;
 CREATE TABLE IF NOT EXISTS `acheteur` (
   `ID` int(11) NOT NULL,
-  `adresse_1` varchar(255) NOT NULL,
-  `adresse_2` varchar(255) NOT NULL,
-  `ville` varchar(255) NOT NULL,
+  `adresse_1` varchar(60) NOT NULL,
+  `adresse_2` varchar(60) NOT NULL,
+  `ville` varchar(30) NOT NULL,
   `code_postal` int(5) NOT NULL,
-  `pays` varchar(255) NOT NULL,
+  `pays` varchar(30) NOT NULL,
   `numero_tel` int(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -47,7 +47,8 @@ CREATE TABLE IF NOT EXISTS `acheteur` (
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `ID` int(11) NOT NULL
+  `ID` int(11) NOT NULL,
+  KEY `fk_admin_id` (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -55,9 +56,9 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 INSERT INTO `admin` (`ID`) VALUES
+(1),
 (2),
-(3),
-(1);
+(3);
 
 -- --------------------------------------------------------
 
@@ -68,13 +69,13 @@ INSERT INTO `admin` (`ID`) VALUES
 DROP TABLE IF EXISTS `identification`;
 CREATE TABLE IF NOT EXISTS `identification` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `pseudo` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `pseudo` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `nom` varchar(30) DEFAULT NULL,
+  `prenom` varchar(30) DEFAULT NULL,
   UNIQUE KEY `ID` (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `identification`
@@ -94,9 +95,9 @@ INSERT INTO `identification` (`ID`, `email`, `pseudo`, `password`, `nom`, `preno
 DROP TABLE IF EXISTS `info_paiement`;
 CREATE TABLE IF NOT EXISTS `info_paiement` (
   `ID` int(11) NOT NULL,
-  `type_carte` varchar(255) NOT NULL,
+  `type_carte` varchar(30) NOT NULL,
   `numero_carte` varchar(16) NOT NULL,
-  `nom_carte` varchar(255) NOT NULL,
+  `nom_carte` varchar(30) NOT NULL,
   `date_expiration` date NOT NULL,
   `code` int(3) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -110,8 +111,8 @@ CREATE TABLE IF NOT EXISTS `info_paiement` (
 DROP TABLE IF EXISTS `vendeur`;
 CREATE TABLE IF NOT EXISTS `vendeur` (
   `ID` int(11) NOT NULL,
-  `img_profil` varchar(255) NOT NULL,
-  `img_fond` varchar(255) NOT NULL
+  `img_profil` varchar(30) NOT NULL,
+  `img_fond` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
