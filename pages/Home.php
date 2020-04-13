f57bd03562e18b942ab90fd9db5ed2c070e212fe
Pierre W <pierre.woj@hotmail.fr>
2020-04-13 18:22:34 +0200

    Home page V1

---
+++ pages/Home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <script src="https://kit.fontawesome.com/c8438c3908.js" crossorigin="anonymous"></script>
+    <script src='main.js'></script>
+    <title>Home</title>
+</head>
+
+<body>
+    <header>
+        Le header
+    </header>
+
+    <div class="container">
+        <img src="../images/boussole.gif" class="bousole"></img>
+        <div class="pgDown animateInf"><i class="fas fa-arrow-down"></i></div>
+        <ul class="animate">
+            <li>Login</li>
+            <li>Buy</li>
+            <li>Sell</li>
+        </ul>
+    </div>
+</body>
+
+</html>
