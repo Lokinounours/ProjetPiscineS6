9d3644f95c426aeee2f60a849a901aebb213f0f9
Pierre W <pierre.woj@hotmail.fr>
2020-04-14 12:20:15 +0200

    Home page almost Done V1.1

--- pages/Home.php
+++ pages/Home.php
@@ -12,17 +12,76 @@
 </head>
 
 <body>
-    <header>
-        Le header
-    </header>
-
     <div class="container">
         <img src="../images/boussole.gif" class="bousole"></img>
-        <div class="pgDown animateInf"><i class="fas fa-arrow-down"></i></div>
+        <div class="pgDown animateInf"><a href="#categorie"><i class="fas fa-arrow-down"></i></a></div>
         <ul class="animate">
             <li>Login</li>
             <li>Buy</li>
             <li>Sell</li>
+        </ul>
+    </div>
+    <div class="categorie" id="categorie">
+        <div class="content-categorie">
+            <div class="content-top">
+                <ul>
+                    <li>categorie</li>
+                    <li>categorie</li>
+                    <li>categorie</li>
+                </ul>
+                <img src="../images/fusée.gif" alt="Une fusée">
+            </div>
+            <div class="content-bottom">
+                <li>
+                    <img src="../images/trésor.png" alt="trésor">
+                    <h3>Trésor</h3>
+                </li>
+                <li>
+                    <img src="../images/musée.png" alt="statue de la liberté">
+                    <h3>Musée</h3>
+                </li>
+                <li>
+                    <img src="../images/accessoire-vip.png" alt="montre de luxe">
+                    <h3>Accésoire VIP</h3>
+                </li>
+            </div>
+
+        </div>
+    </div>
+    <div class="presentation">
+        <h1>Commander Simplement</h1>
+        <ul>
+            <li>
+                <img src="../images/quickly.gif" alt="Rapidité">
+                <div>
+                    <h3>Rapidité</h3>
+                    <p>Nos livraisons sont assurées en 48h ouvrées. Nos partenaires vous permettent de profiter de
+                        l'ensemble des systèmes de livraisons : à votre domicile, ou dans vos commerces de proximité.
+
+                        * En raison de la pandémie du covid-19 : certaines livraisons non prioritaires peuvent être
+                        retardées.</p>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <h3>Efficacité</h3>
+                    <p>Trouvez tous les produits que vous souhaitez grace à notre grande variété de choix. Un conseiller
+                        peut vous aider dans la réalisation de votre commande ou pour répondre à vos questions dans le
+                        formulaire de contact en bas de page.</p>
+                </div>
+                <img src="../images/efficiently.gif" alt="Efficacité">
+            </li>
+            <li>
+                <img src="../images/safely.gif" alt="Sécurité">
+                <div>
+                    <h3>Sécurité</h3>
+                    <p>Notre processus de paiement est sécurisé. A chaque commande que vous effectuez vous êtes protégé.
+                        Vos
+                        cartes de crédits ne sont pas enregistrées ainsi personne ne peut usurper votre identité
+                        bancaire.
+                    </p>
+                </div>
+            </li>
         </ul>
     </div>
 </body>
