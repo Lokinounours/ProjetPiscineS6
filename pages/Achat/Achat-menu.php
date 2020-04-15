a6ad73962eb6bb188caa92d9f5b24a29fc00a001
Pierre W <pierre.woj@hotmail.fr>
2020-04-15 16:20:03 +0200

    Achat v1

--- pages/Achat/Achat-menu.php
+++ pages/Achat/Achat-menu.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='achat-style.css'>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel='stylesheet' type='text/css' media='screen' href='achat-style.css'>
 	<title>Achat Menu</title>
 </head>
+
 <body>
 
 	<div class="container">
-
-		<div class="titre">
-			
-
-			<div class="Achat-bottom">
-				<h1>ACHAT</h1>
-				<ul>
+		<div class="achat-bottom">
+			<h1>ACHAT</h1>
+			<ul>
 				<li>
 					<img src="../../images/Logo/logo-enchere.png">
 					<h3>ENCHERES</h3>
@@ -29,9 +27,9 @@
 					<img src="../../images/Logo/logo-meilleure-offre.png">
 					<h3>MEILLEURES OFFRES</h3>
 				</li>
-				</ul>
-			</div>
+			</ul>
 		</div>
 	</div>
 </body>
+
 </html>
