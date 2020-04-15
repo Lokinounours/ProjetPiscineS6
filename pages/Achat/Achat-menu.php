87d8568f86f301a41ddcde8dd830e2af6d54c9e7
Lestrade <jules.lestrade@edu.ece.fr>
2020-04-15 16:03:21 +0200

    Achat page v1

---
+++ pages/Achat/Achat-menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='achat-style.css'>
+	<title>Achat Menu</title>
+</head>
+<body>
+
+	<div class="container">
+
+		<div class="titre">
+			
+
+			<div class="Achat-bottom">
+				<h1>ACHAT</h1>
+				<ul>
+				<li>
+					<img src="../../images/Logo/logo-enchere.png">
+					<h3>ENCHERES</h3>
+				</li>
+				<li>
+					<img src="../../images/Logo/logo-panier2.png">
+					<h3>ACHAT IMMÉDIAT</h3>
+				</li>
+				<li>
+					<img src="../../images/Logo/logo-meilleure-offre.png">
+					<h3>MEILLEURES OFFRES</h3>
+				</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
