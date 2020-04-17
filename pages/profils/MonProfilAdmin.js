df83f8d6c64783d12d797d4773b402da84f6dd57
Pierre W <pierre.woj@hotmail.fr>
2020-04-17 15:57:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- pages/profils/MonProfilAdmin.js
+++ pages/profils/MonProfilAdmin.js
@@ -9,9 +9,22 @@ $(document).ready(function () {
     //         })
     //     }
     // });  
-    $(".listMarchandises i").click(function (event) {
-        console.log(this.id);
+    $("i").click(function (event) {
+        $("i").each(function (index) {
+            this.className = "fas fa-times"
+        })
+
+        console.log(this.className);
+        (this.className == "fas fa-check") ? (
+            this.className = "fas fa-times"
+            ) : (
+                this.className = "fas fa-check"
+            )
+
         let myhidden = document.getElementById("hiddenID");
         myhidden.value = this.id;
     });
 });
+
+// 
+// fas fa-times
