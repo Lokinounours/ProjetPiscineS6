d1ee6cc6eb747aeeec5729ff743cb4df55b9ae69
Paulcou <paul.coutiere@hotmail.fr>
2020-04-18 15:32:43 +0200

    update detailAchat

--- pages/Achat/detailAchat.php
+++ pages/Achat/detailAchat.php
@@ -1,40 +1,42 @@
 <?php
-    // session_start();
-    // $idVendeur = $_SESSION['id'];
-
-    // $idVendeur = 60;
-
-    // $database = "piscine";
-    // $db_handle = mysqli_connect('localhost', 'root', '');
-    // $db_found = mysqli_select_db($db_handle, $database);
-
-    // if ($db_found) {
-    //     $sql = "SELECT * FROM item WHERE IDprop = $idVendeur";
-    //     $result = mysqli_query($db_handle, $sql);
-
-    //     $sql = "SELECT * FROM vendeur WHERE ID = $idVendeur";
-    //     $result2 = mysqli_query($db_handle, $sql);
-
-    //     $imgProfil = "../../images/Avatar/";
-    //     $imgFond = "../../images/Fond/";
-    //     while ($data2 = mysqli_fetch_assoc($result2)){
-    //         $imgProfil .= $data2["img_profil"];
-    //         $imgFond .= $data2["img_fond"];
-    //     }
-
-    //     $sql = "SELECT * FROM identification WHERE ID = $idVendeur";
-    //     $result3 = mysqli_query($db_handle, $sql);
-    //     while ($data3 = mysqli_fetch_assoc($result3)){
-    //         $pseudo = $data3["pseudo"];
-    //     }
-    // }
-
-    // mysqli_close($db_handle);
+    session_start();
+    $idAcheteur = $_SESSION['id'];
+    $idProduit = $_SESSION['idProduit'];
 
     if (!empty($_REQUEST["hiddenPrix"]))$prix = $_REQUEST["hiddenPrix"];
     else $prix = 0;
-    
-    echo $prix;
+
+    $database = "piscine";
+    $db_handle = mysqli_connect('localhost', 'root', '');
+    $db_found = mysqli_select_db($db_handle, $database);
+
+    if ($db_found) {
+        $sql = "SELECT * FROM item WHERE ID = $idProduit";
+        $result = mysqli_query($db_handle, $sql);
+        while($data = mysqli_fetch_assoc($result)){
+            $nomItem = $data['nom'];
+            $descriptionItem = $data['description'];
+            $photoItem = $data['photo'];
+            $nomItem = $data['nom'];
+            $videoItem = $data['video'];
+            $prixItem = $data['prix'];
+            $categorieItem = $data['categorie'];
+            $idProp = $data['IDprop'];
+            $etat = $data['etat'];
+        }
+
+        $sql = "SELECT * FROM identification WHERE ID = $idProp";
+        $result2 = mysqli_query($db_handle, $sql);
+
+        while($data = mysqli_fetch_assoc($result2)){
+            $pseudoVendeur = $data['pseudo'];
+        }
+
+        $sql = "SELECT * FROM vendeur WHERE ID = $idProp";
+        $result3 = mysqli_query($db_handle, $sql);
+    }
+
+    mysqli_close($db_handle);
 ?>
 
 <html>
@@ -67,56 +69,63 @@
         <div class="fondVendre" style="background-image: url(../../images/Fond/fond-choix3.jpg);">
             <p>Achat</p>
             <div class="carteVendeur">
-                <div class="bckgdCarteVendeur" style="background-image: url('../../images/Fond/fond-choix1.jpg');">
-                </div>
-                <div class="imgVendeur">
-                    <img src="../../images/Avatar/avatar-Compte.png">
-                </div>
-                <div class="psdVendeur">
-                    <p>Piotr</p>
-                </div>
+                <?php
+                    while($data = mysqli_fetch_assoc($result3)){
+                        echo '<div class="bckgdCarteVendeur" style="background-image: url("../../images/Fond/'. $data['img_fond'] . '");">';
+                        echo '</div>';
+                        echo '<div class="imgVendeur">';
+                        echo '<img src="../../images/Avatar/' . $data['img_profil'] .'">';
+                        echo '</div>';
+                    }
+                
+                    echo '<div class="psdVendeur">';
+                    echo '<p>' . $pseudoVendeur .'</p>';
+                    echo '</div>';
+                ?>
             </div>
         </div>
     </div>
     <div class="detail-item">
         <div class="top">
-            <div class="left"><img src="../../images/Items/6.png" alt=""></div>
-            <div class="right">
-                <h1>Musée</h1>
-                <p>Il date d'avant JC ce ouf.</p>
-            </div>
+            <?php
+                echo '<div class="left"><img src="../../images/Items/' . $photoItem . '" alt=""></div>';
+                echo '<div class="right">';
+                echo '<h1>' . $categorieItem . '</h1>';
+                echo '<p>' . $descriptionItem . '</p>'
+                echo '</div>';
+            ?>
         </div>
         <div class="bottom">
             <!-- ID en fonction du type de vente -->
             <!-- if etat == E -->
-            <div class="elem" id="E">
-                <div class="left"><img src="../../images/Logo/logo-enchere.png" alt=""></div>
-                <div class="right">
-                    <h1>Enchère</h1>
-                    <!-- ID en fonction du type de vente -->
-                    <p id="E">40£</p>
+                <div class="elem" id="E">
+                    <div class="left"><img src="../../images/Logo/logo-enchere.png" alt=""></div>
+                    <div class="right">
+                        <h1>Enchère</h1>
+                        <!-- ID en fonction du type de vente -->
+                        <p id="E">40£</p>
+                    </div>
                 </div>
-            </div>
-            <!-- ID en fonction du type de vente -->
-            <!-- if etat == I -->
-            <div class="elem" id="I"> 
-                <div class="left"><img src="../../images/Logo/logo-achat-imédiat.png" alt=""></div>
-                <div class="right">
-                    <h1>Achat immédiat</h1>
-                    <!-- ID en fonction du type de vente -->
-                    <p id="I">85£</p>
+                <!-- ID en fonction du type de vente -->
+                <!-- if etat == I -->
+                <div class="elem" id="I"> 
+                    <div class="left"><img src="../../images/Logo/logo-achat-imédiat.png" alt=""></div>
+                    <div class="right">
+                        <h1>Achat immédiat</h1>
+                        <!-- ID en fonction du type de vente -->
+                        <p id="I">85£</p>
+                    </div>
                 </div>
-            </div>
-            <!-- ID en fonction du type de vente -->
-            <!-- if etat == M -->
-            <div class="elem" id="M"> 
-                <div class="left"><img src="../../images/Logo/logo-meilleure-offre.png" alt=""></div>
-                <div class="right">
-                    <h1>Meilleure offre</h1>
-                     <!-- ID en fonction du type de vente -->
-                    <p id="M">21£</p>
+                <!-- ID en fonction du type de vente -->
+                <!-- if etat == M -->
+                <div class="elem" id="M"> 
+                    <div class="left"><img src="../../images/Logo/logo-meilleure-offre.png" alt=""></div>
+                    <div class="right">
+                        <h1>Meilleure offre</h1>
+                        <!-- ID en fonction du type de vente -->
+                        <p id="M">21£</p>
+                    </div>
                 </div>
-            </div>
         </div>
     </div>
 </body>
