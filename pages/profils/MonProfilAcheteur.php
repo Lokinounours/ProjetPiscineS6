88ac2b5751f2f0e4b33fb15635b6ca8487471dab
Pierre W <pierre.woj@hotmail.fr>
2020-04-19 18:02:41 +0200

    pushing

--- pages/profils/MonProfilAcheteur.php
+++ pages/profils/MonProfilAcheteur.php
@@ -3,7 +3,7 @@
 
     $database = "piscine";
 
-    $db_handle = mysqli_connect('localhost', 'root', 'root');
+    $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
 
     $pseudo = $_SESSION['pseudo'];
